588fe9e7047c182f8dd44997bd8e63aa2c77e132
iii-koyanagi <koyanagi@iii-store.com>
2015-06-23 17:58:50 +0900

    総入れ替え

--- src/Calculate.php
+++ src/Calculate.php
@@ -13,21 +13,6 @@ class Calculate {
 
     public function calculate($distanceValue)
     {
-        foreach ($distanceValue as $key => $value) {
-            if ($key === 0) {
-                if ($value[1] === true) {
-                    $totalPrice = 400;
-                    $startingFare = 995;
-                    $addingPrice = 60;
-                    $calArr = array($totalPrice, $startingFare, $addingPrice);
-                }
-                else {
-                    $totalPrice = 350;
-                    $startingFare = 845;
-                    $addingPrice = 50;
-                    $calArr = array($totalPrice, $startingFare, $addingPrice);
-                }
-            }
-        }
+
     }
 }
---
+++ tests/DistanceTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace TripleI\Distance;
+
+use TripleI\taxi\Distance;
+
+class DistanceTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var distance
+     */
+    protected $skeleton;
+
+    protected function setUp()
+    {
+        parent::setUp();
+        $this->skeleton = new Distance();
+    }
+
+    public function testDistance()
+    {
+        $data = 'ADFC';
+        $distance = new Distance();
+        $sectionArray = $distance->sectionArray($data);
+        $distanceValue = $distance->getDistanceValue($sectionArray);
+
+        var_dump($distanceValue);
+    }
+}
